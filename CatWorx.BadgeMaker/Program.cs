424274e4133d1a6ffef509c24184e77a05cfbd0f
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-05-01 17:20:19 -0400

    priting all data

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -36,10 +36,10 @@ int id = Int32.Parse(Console.ReadLine() ?? "");
 
   static void PrintEmployees(List<Employee> employees)
   {
-  for (int i = 0; i < employees.Count; i++) 
+for (int i = 0; i < employees.Count; i++) 
 {
-  // each item in employees is now an Employee instance
-  Console.WriteLine(employees[i].GetFullName());
+  string template = "{0,-10}\t{1,-20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
 }
   }
 
