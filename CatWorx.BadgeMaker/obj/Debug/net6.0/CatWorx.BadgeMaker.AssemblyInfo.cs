ab3fb0af02a185d7bdcbc122e8e8fa5d425b5c7c
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-05-09 16:37:44 -0400

    render employee photo

--- CatWorx.BadgeMaker/obj/Debug/net6.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ CatWorx.BadgeMaker/obj/Debug/net6.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa924b4b77e05d2015c0959ad628cab6b25bdec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9aeeb6cd17ab9b08e9360029b669d88843f328")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
